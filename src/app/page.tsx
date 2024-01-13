e8c7d2dbc1305c45dcbd257e1f87c4c8077a5b61
imopbuilder <opbuilder9@gmail.com>
2024-01-13 12:49:29 +0530

    enhance: update home page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import Header from '@/components/global/header';
-import Home from '@/components/pages/home';
+import TelNumberForm from '@/components/pages/home/client';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
-export default function Page() {
+export default function page() {
 	return (
-		<>
+		<Fragment>
 			<Header />
-			<Home />
-		</>
+			<main>
+				<section>
+					<div className='w-maxi mx-auto py-5 min-h-dvh flex items-center justify-center flex-col'>
+						<div className='w-full'>
+							<TelNumberForm />
+						</div>
+						<div className='h-full mt-auto w-full'>
+							<Button className='w-full py-9 rounded-full' variant='outline' type='button' asChild>
+								<Link href='/history'>History</Link>
+							</Button>
+						</div>
+					</div>
+				</section>
+			</main>
+		</Fragment>
 	);
 }
