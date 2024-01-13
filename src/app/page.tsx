081e5e5dafc966708fbdef9af548e5f122a6ca3b
imopbuilder <opbuilder9@gmail.com>
2024-01-13 17:27:33 +0530

    feat: add hero section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,14 @@ export default function page() {
 			<Header />
 			<main>
 				<section>
-					<div className='max-w-maxi mx-auto py-12 px-4 md:px-5 min-h-dvh flex items-start justify-center'>
+					<div className='max-w-maxi mx-auto py-10 px-4 md:px-5 min-h-dvh flex items-start justify-center'>
 						<div className='w-full max-w-2xl'>
+							<div className='pb-6'>
+								<h2 className='text-2xl font-bold'>Start chating</h2>
+								<p className='text-sm text-muted-foreground pt-1.5'>
+									Chat with anyone in <strong>WhatsApp</strong> without saving number.
+								</p>
+							</div>
 							<TelNumberForm />
 							<Button className='w-full mt-5 font-bold' variant='outline' size='lg' type='button' asChild>
 								<Link href='/history'>
