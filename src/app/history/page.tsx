a629438df91876be2a945739610cb194213e617c
imopbuilder <opbuilder9@gmail.com>
2024-01-13 13:53:29 +0530

    styles and enhance: update the styles for the contact card component

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -13,8 +13,8 @@ export default function page() {
 			<Header />
 			<main>
 				<section>
-					<div className='w-maxi mx-auto py-5'>
-						<div className='flex items-center justify-between mb-3'>
+					<div className='max-w-2xl mx-auto py-5'>
+						<div className='flex items-center justify-between mb-3 border-b pb-3'>
 							<h2 className='text-2xl font-bold'>History</h2>
 							<DeleteContactBtn>
 								<Trash2 size={18} className='scale-100 hover:scale-110 duration-200' />
--- src/components/pages/history/client.tsx
+++ src/components/pages/history/client.tsx
@@ -3,9 +3,9 @@
 import { Whatsapp } from '@/components/custom/svg/whatsapp';
 import { Button } from '@/components/ui/button';
 import { WHATS_APP_URL } from '@/constants/wa-chat';
-import { setLocalStorage } from '@/lib/utils/local-storage';
+import { getLocalStorage, setLocalStorage } from '@/lib/utils/local-storage';
 import { Phone } from 'lucide-react';
-import { Fragment, ReactNode, useState } from 'react';
+import { Fragment, ReactNode, useEffect, useState } from 'react';
 
 export function DeleteContactBtn({ children }: { children: ReactNode }) {
 	return (
@@ -17,31 +17,37 @@ export function DeleteContactBtn({ children }: { children: ReactNode }) {
 
 export function Contacts() {
 	const [contacts, setContacts] = useState<string[]>([]);
-	// const localContacts = getLocalStorage('contacts', JSON.stringify([]));
 
-	// if (!localContacts) return null;
+	useEffect(() => {
+		const localContacts = getLocalStorage('contacts', JSON.stringify([]));
 
-	// const parsedContacts: string[] = JSON.parse(localContacts);
+		if (!localContacts) return;
 
-	// useEffect(() => {
-	// 	setContacts(parsedContacts);
-	// }, [parsedContacts]);
+		const parsedContacts: string[] = JSON.parse(localContacts);
+		setContacts(parsedContacts);
+	}, []);
 
 	return (
 		<Fragment>
 			{contacts.map((contact, index) => (
-				<div key={`${index}`} className='py-3 font-aldrich rounded-full flex items-center justify-between'>
-					<p>+{contact}</p>
-					<div className='flex items-center justify-center gap-5'>
-						<a href={`${WHATS_APP_URL}/${contact}?text=Hey...`} className='hover:scale-105 duration-200' target='_blank' rel='noreferrer'>
-							<Whatsapp />
-						</a>
-						<a href={`tel:${contact}`} className='hover:scale-105 duration-200' target='_blank' rel='noreferrer'>
-							<Phone size={18} className='stroke-foreground' />
-						</a>
-					</div>
-				</div>
+				<ContactCard key={`${index}`} contact={contact} />
 			))}
 		</Fragment>
+	);
+}
+
+function ContactCard({ contact }: { contact: string }) {
+	return (
+		<div className='py-2.5 px-3.5 mb-4 last:mb-0 bg-muted text-sm font-aldrich rounded-lg border flex items-center justify-between'>
+			<p>+{contact}</p>
+			<div className='flex items-center justify-center gap-5'>
+				<a href={`${WHATS_APP_URL}/${contact}?text=Hey...`} className='hover:scale-105 duration-200' target='_blank' rel='noreferrer'>
+					<Whatsapp />
+				</a>
+				<a href={`tel:${contact}`} className='hover:scale-105 duration-200' target='_blank' rel='noreferrer'>
+					<Phone size={18} className='stroke-foreground' />
+				</a>
+			</div>
+		</div>
 	);
 }
